e4a0777d0275d4214d811db5bc53d064dd464937
YOUNGMIN OH <youngmin.oh@yonsei.ac.kr>
2022-11-04 18:50:42 +0900

    Update step1.sh

--- scripts/ade/step1.sh
+++ scripts/ade/step1.sh
@@ -3,9 +3,6 @@ TASK=100-50 # Select one of {100-50, 50, 100-10}
 STEP=1      # Type a valid step (> 0)
 GPUS=0,1    # Type gpu indices
 
-if [ ${STEP} -eq 1 ]; then
-  CONFIG=configs/${DATA}/${TASK}/${STEP}_step1.yml
-else # STEP > 1
-  CONFIG=configs/${DATA}/${TASK}/M${STEP}_step1.yml
-fi
+CONFIG=configs/${DATA}/${TASK}/${STEP}_step1.yml # For ALIFE
+#CONFIG=configs/${DATA}/${TASK}/M${STEP}_step1.yml # For ALIFE-M
 CUDA_VISIBLE_DEVICES=${GPUS} python run_step1.py --config-file ${CONFIG}
