f23a81803b142c55a67ab38376598db295940ea0
YOUNGMIN OH <youngmin.oh@yonsei.ac.kr>
2022-11-04 18:49:51 +0900

    Update step1.sh

--- scripts/voc/step1.sh
+++ scripts/voc/step1.sh
@@ -3,9 +3,6 @@ TASK=15-5 # Select one of {19-1, 15-5, 15-5s}
 STEP=1    # Type a valid step (> 0)
 GPUS=0,1  # Type gpu indices
 
-if [ ${STEP} -eq 1 ]; then
-  CONFIG=configs/${DATA}/${TASK}/${STEP}_step1.yml
-else # STEP > 1
-  CONFIG=configs/${DATA}/${TASK}/M${STEP}_step1.yml
-fi
+CONFIG=configs/${DATA}/${TASK}/${STEP}_step1.yml # For ALIFE
+#CONFIG=configs/${DATA}/${TASK}/M${STEP}_step1.yml # For ALIFE-M
 CUDA_VISIBLE_DEVICES=${GPUS} python run_step1.py --config-file ${CONFIG}
